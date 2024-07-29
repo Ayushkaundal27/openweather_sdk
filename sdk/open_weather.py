973f1c337ff4f163741f932710fcf24eaae59c1f
Ayush kaundal <ayushkaundal27@gmail.com>
2024-07-29 11:27:27 +0530

    added weather forecast endpoint

--- sdk/open_weather.py
+++ sdk/open_weather.py
@@ -1,4 +1,5 @@
 from sdk.openweather.current_weather_function.current_weather import CurrentWeather 
+from sdk.openweather.forecast.forecast import WeatherForecast
 
 
 class OpenWeather:
@@ -7,3 +8,6 @@ class OpenWeather:
 
     def current_weather(self, lat:str = 0, log:str = 0, unit:str = None, lang:str = None, city:str = None, mode_of_response:str = None):
         return CurrentWeather(lat, log, self.api_key, unit, lang,city, mode_of_response).current_weather().text
+     
+    def weather_forecast(self, lat:str = 0, log:str = 0, unit:str = None, lang:str = None, city:str = None, mode_of_response:str = None):
+        return WeatherForecast(lat, log, self.api_key, unit, lang,city, mode_of_response).weather_forecast().text
--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from sdk.open_weather import OpenWeather 
 
 
-p=OpenWeather("8560bfec3d03f64cbcc8eae684aa37da").current_weather(0,0,unit="metric",city="delhi",mode_of_response="html")
-print(p)
+c=OpenWeather("8560bfec3d03f64cbcc8eae684aa37da").current_weather(0,0,unit="metric",city="delhi",mode_of_response="html")
+f=OpenWeather("8560bfec3d03f64cbcc8eae684aa37da").weather_forecast(0,0,unit="metric",city="delhi",mode_of_response="xml")
+print(c,f)
